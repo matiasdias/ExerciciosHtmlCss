d41c9e9e9c33f643bc850dabbdd0aab5dd0126ea
matiasdias <39205071+matiasdias@users.noreply.github.com>
2022-02-21 16:23:32 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,50 @@
+//getElementById()
+//.innerHTML = 'texto-teste'
+//const form = document.getElementById('negrito')
+//console.log(form)
+
+
+//getElementyByClassNme()
+
+/*const form =  document.getElementsByClassName('formulario')
+console.log(form)*/
+
+//getEmentyByTagName()
+
+//querySelector()
+
+/*const form = document.querySelector('.labelInput')
+console.log(form)*/
+
+//querySelectorAll()
+
+//manipulando conteudo 
+//textContent
+
+/*const form = document.querySelector('legend')
+
+form.textContent = ' Olá devs!'*/
+
+//manipular valores via input
+
+/*const form = document.querySelector('input')
+
+form.value = "digite seu nome"**/
+
+
+//manipulando elementos atributos
+
+const head = document.querySelector('head')
+head.setAttribute('id', 'head')
+
+const headID = document.querySelector('#head')
+console.log(headID.getAttribute())
+
+head.removeAttribute()
+ 
+//alterando estilos 
+
+
+
+
+
